a32f395c583ff66b8224f2b20a6777d9ca1cfb9c
Emprestimo <emprestimo@Emprestimos-MacBook-Pro.local>
2019-03-24 11:10:24 -0300

    muda o retorno da função hello_world de string para objeto json

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify(string_teste='Hello, World!')
 
 if __name__ == '__main__':
     app.run()
