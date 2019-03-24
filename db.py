6f8f59cbfd8655595175f642a1f802a5af2ba6e1
brenda <brendasalenave@live.com>
2019-03-24 16:43:14 -0300

    altera caminho db (de novo)

--- db.py
+++ db.py
@@ -6,4 +6,4 @@ MONGODB_URL = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017')
 MONGODB_COLLECTION = os.environ.get('MONGODB_COLLECTION', 'agcompromisso')
 
 def get_connection():
-  return MongoClient(MONGODB_URL)["agcompromisso"]
+  return MongoClient(MONGODB_URL)[MONGODB_COLLECTION]
