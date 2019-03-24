3a9ffd996c27b4ae213626c684fbce766d02edbb
brenda <brendasalenave@live.com>
2019-03-24 16:15:42 -0300

    alterado caminho no db

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import os
 from pymongo import MongoClient
 
-MONGODB_URL = os.environ.get('MONGODB_URL', 'mongodb://localhost:27017')
+MONGODB_URL = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017')
 
 def get_connection():
   return MongoClient(MONGODB_URL)["agcompromisso"]
