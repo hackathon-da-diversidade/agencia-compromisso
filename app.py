194a2a62c09a49e2fa8e8120b6c704b5a77b78e0
Maurício Dias <mau.dias@gmail.com>
2019-03-24 11:16:45 -0300

    Cria esqueleto das primeiras rotas na app

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify(string_teste='Hello, World!')
+
+@app.route('/list', methods=['GET', 'POST'])
+def list():
+    if request.method == 'GET':
+        # Apenas lista o conteúdo
+        pass
+    elif request.method == 'POST':
+        # Filtra o conteúdo antes de listar
+        pass
+
+@app.route('/create', methods=['POST'])
+def create():
+    pass
 
 if __name__ == '__main__':
     app.run()
