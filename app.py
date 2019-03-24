79cd42120d3ba5560dc7b68cc5435f6f39dc48f2
brenda <brendasalenave@live.com>
2019-03-24 11:20:52 -0300

    conecta a aplicaçao ao bd

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import os
+from pymongo import MongoClient
 from flask import Flask
 from flask import jsonify
 from flask import request
 app = Flask(__name__)
 
+MONGODB_URL = os.environ.get('MONGODB_URL', 'mongodb://localhost:27017')
+
+def get_connection():
+  return MongoClient(MONGODB_URL)["agcompromisso"]
+
 @app.route('/')
 def hello_world():
+    get_connection()
     return jsonify(string_teste='Hello, World!')
 
 @app.route('/list', methods=['GET', 'POST'])
