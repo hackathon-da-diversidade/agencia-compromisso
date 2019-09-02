507bb707917c11d9784eab42df1a51cf9455edd6
Thayse Onofrio <thayseonofrio@hotmail.com>
2019-09-02 16:56:51 -0300

    refatora criar modelo

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import os
-from pymongo import MongoClient
 from flask import Flask
-from flask import jsonify
 from flask import request
 from flask import Response
-from flask_cors import CORS
 import json
 from bson.json_util import default
 from list_function import list_all
@@ -12,8 +8,6 @@ from cadastro import Modelo
 from db import get_connection
 
 app = Flask(__name__)
-cors = CORS(app)
-MONGODB_URL = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017')
 
 @app.route('/list', methods=['GET', 'POST'])
 def list():
@@ -28,10 +22,25 @@ def list():
 def create():
     requestData = request.get_json()
 
-    nome = requestData.get('nome',None)
-    data_nascimento = requestData.get('data_nascimento',None)
-    email = requestData.get('email',None)
-    telefone = requestData.get('telefone',None)
+    novo_modelo = criar_modelo(requestData)
+
+    collection = 'modelos'
+    connection = get_connection()[collection]
+
+    resultado = connection.insert_one(novo_modelo)
+
+    if resultado.inserted_id:
+        return Response(json.dumps(novo_modelo, default=default), status=201, mimetype='application/json')
+
+    return Response(status=400)
+
+
+def criar_modelo(requestData):
+
+    nome = requestData.get('nome', None)
+    data_nascimento = requestData.get('data_nascimento', None)
+    email = requestData.get('email', None)
+    telefone = requestData.get('telefone', None)
     endereco_logradouro = requestData.get('endereco_logradouro', None)
     endereco_numero = requestData.get('endereco_numero', None)
     endereco_complemento = requestData.get('endereco_complemento', None)
@@ -39,32 +48,32 @@ def create():
     endereco_cidade = requestData.get('endereco_cidade', None)
     endereco_estado = requestData.get('endereco_estado', None)
     endereco_cep = requestData.get('endereco_cep', None)
-    responsavel_nome = requestData.get('responsavel_nome',None)
-    responsavel_nascimento = requestData.get('responsavel_nascimento',None)
-    responsavel_email = requestData.get('responsavel_email',None)
-    responsavel_telefone = requestData.get('responsavel_telefone',None)
-    genero = requestData.get('genero',None)
-    lgbtqia = requestData.get('lgbtqia',None)
-    escolaridade = requestData.get('escolaridade',None)
-    ocupacao = requestData.get('ocupacao',None)
-    moradia = requestData.get('moradia',None)
-    moradores = requestData.get('moradores',None)
-    filhos = requestData.get('filhos',None)
-    etnia = requestData.get('etnia',None)
-    renda = requestData.get('renda',None)
-    medida_altura = requestData.get('medida_altura',None)
-    medida_busto = requestData.get('medida_busto',None)
-    medida_cintura = requestData.get('medida_cintura',None)
-    medida_quadril = requestData.get('medida_quadril',None)
-    observacoes = requestData.get('observacoes',None)
+    responsavel_nome = requestData.get('responsavel_nome', None)
+    responsavel_nascimento = requestData.get('responsavel_nascimento', None)
+    responsavel_email = requestData.get('responsavel_email', None)
+    responsavel_telefone = requestData.get('responsavel_telefone', None)
+    genero = requestData.get('genero', None)
+    lgbtqia = requestData.get('lgbtqia', None)
+    escolaridade = requestData.get('escolaridade', None)
+    ocupacao = requestData.get('ocupacao', None)
+    moradia = requestData.get('moradia', None)
+    moradores = requestData.get('moradores', None)
+    filhos = requestData.get('filhos', None)
+    etnia = requestData.get('etnia', None)
+    renda = requestData.get('renda', None)
+    medida_altura = requestData.get('medida_altura', None)
+    medida_busto = requestData.get('medida_busto', None)
+    medida_cintura = requestData.get('medida_cintura', None)
+    medida_quadril = requestData.get('medida_quadril', None)
+    observacoes = requestData.get('observacoes', None)
 
-    mod = get_connection()['modelos']
+    novo_modelo = Modelo(nome, data_nascimento, email, telefone, endereco_logradouro, endereco_numero,
+                         endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado, endereco_cep, \
+                         responsavel_nome, responsavel_nascimento, responsavel_email, responsavel_telefone, genero,
+                         lgbtqia, escolaridade, ocupacao, moradia, moradores, filhos, etnia, renda, \
+                         medida_altura, medida_busto, medida_cintura, medida_quadril, observacoes)
+    return novo_modelo
 
-    result = Modelo(nome, data_nascimento, email, telefone, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado, endereco_cep,\
-                    responsavel_nome, responsavel_nascimento, responsavel_email, responsavel_telefone, genero, lgbtqia, escolaridade, ocupacao, moradia, moradores, filhos, etnia, renda,\
-                    medida_altura, medida_busto, medida_cintura, medida_quadril, observacoes)
-    mod.insert(result)
-    return Response(json.dumps(result, default=default), status=201, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
