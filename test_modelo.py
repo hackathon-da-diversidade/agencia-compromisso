0e40426ba232d6a8a41b8ded7b5225ed9415c22b
Thayse Onofrio <thayseonofrio@hotmail.com>
2019-09-27 12:03:56 -0300

    Adiciona setup

--- test_modelo.py
+++ test_modelo.py
@@ -4,80 +4,86 @@ from modelo import Modelo
 
 class TestModelo(TestCase):
 
-    def test_modelo(self):
-        nome = 'Maria'
-        data = '01/01/2000'
-        email = 'maria@hotmail.com'
-        telefone = '33333333'
-        logradouro = 'Avenida ipiranga'
-        endereco_numero = '1010'
-        endereco_complemento = '20'
-        endereco_bairro = 'Partenon'
-        endereco_cidade = 'Porto Alegre'
-        endereco_estado = 'RS'
-        endereco_cep = '90011011'
-        nome_responsavel = 'Responsavel da Maria'
-        nascimento_responsavel = '01/01/1980'
-        email_responsavel = 'responsavel@hotmail.com'
-        telefone_responsavel = '33333333'
-        genero = 'feminino'
-        lgbtqia = 'true'
-        escolaridade = 'ensino_medio'
-        ocupacao = 'estudante'
-        moradia = 'alugada'
-        moradores = '2'
-        filhos = '0'
-        etnia = 'negro'
-        disponibilidade = 'total'
-        renda = '900,00'
-        medida_altura = '160'
-        medida_busto = '90'
-        medida_cintura = '80'
-        medida_quadril = '100'
-        observacoes = 'nenhuma'
+    freezer = freeze_time("2019-09-26 12:00:01")
 
-        modelo = Modelo(nome, data, email, telefone, logradouro,
-                        endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado, endereco_cep,
-                        nome_responsavel,
-                        nascimento_responsavel, email_responsavel, telefone_responsavel, genero, lgbtqia,
-                        escolaridade, ocupacao, moradia, moradores, filhos, etnia, disponibilidade, renda, medida_altura,
-                        medida_busto, medida_cintura, medida_quadril, observacoes)
-        self.assertEqual(modelo.nome, '%s' % nome)
-        self.assertEqual(modelo.data_nascimento, data)
-        self.assertEqual(modelo.email, email)
-        self.assertEqual(modelo.telefone, telefone)
-        self.assertEqual(modelo.endereco_logradouro, logradouro)
-        self.assertEqual(modelo.endereco_numero, endereco_numero)
-        self.assertEqual(modelo.endereco_complemento, endereco_complemento)
-        self.assertEqual(modelo.endereco_bairro, endereco_bairro)
-        self.assertEqual(modelo.endereco_cidade, endereco_cidade)
-        self.assertEqual(modelo.endereco_estado, endereco_estado)
-        self.assertEqual(modelo.endereco_cep, endereco_cep)
-        self.assertEqual(modelo.responsavel_nome, nome_responsavel)
-        self.assertEqual(modelo.responsavel_nascimento, nascimento_responsavel)
-        self.assertEqual(modelo.responsavel_email, email_responsavel)
-        self.assertEqual(modelo.responsavel_telefone, telefone_responsavel)
-        self.assertEqual(modelo.genero, genero)
-        self.assertEqual(modelo.lgbtqia, lgbtqia)
-        self.assertEqual(modelo.escolaridade, escolaridade)
-        self.assertEqual(modelo.ocupacao, ocupacao)
-        self.assertEqual(modelo.moradia, moradia)
-        self.assertEqual(modelo.moradores, moradores)
-        self.assertEqual(modelo.filhos, filhos)
-        self.assertEqual(modelo.etnia, etnia)
-        self.assertEqual(modelo.disponibilidade, disponibilidade)
-        self.assertEqual(modelo.renda, renda)
-        self.assertEqual(modelo.medida_altura, medida_altura)
-        self.assertEqual(modelo.medida_busto, medida_busto)
-        self.assertEqual(modelo.medida_cintura, medida_cintura)
-        self.assertEqual(modelo.medida_quadril, medida_quadril)
-        self.assertEqual(modelo.observacoes, observacoes)
+    def setup_method(self, test_method):
+        print('set up')
+        self.freezer.start()
+
+        self.nome = 'Maria'
+        self.data = '1993-07-09T03:00:00.000Z'
+        self.email = 'maria@hotmail.com'
+        self.telefone = '33333333'
+        self.logradouro = 'Avenida ipiranga'
+        self.endereco_numero = '1010'
+        self.endereco_complemento = '20'
+        self.endereco_bairro = 'Partenon'
+        self.endereco_cidade = 'Porto Alegre'
+        self.endereco_estado = 'RS'
+        self.endereco_cep = '90011011'
+        self.nome_responsavel = 'Responsavel da Maria'
+        self.nascimento_responsavel = '01/01/1980'
+        self.email_responsavel = 'responsavel@hotmail.com'
+        self.telefone_responsavel = '33333333'
+        self.genero = 'feminino'
+        self.lgbtqia = 'true'
+        self.escolaridade = 'ensino_medio'
+        self.ocupacao = 'estudante'
+        self.moradia = 'alugada'
+        self.moradores = '2'
+        self.filhos = '0'
+        self.etnia = 'negro'
+        self.disponibilidade = 'total'
+        self.renda = '900,00'
+        self.medida_altura = '160'
+        self.medida_busto = '90'
+        self.medida_cintura = '80'
+        self.medida_quadril = '100'
+        self.observacoes = 'nenhuma'
+
+        self.modelo = Modelo(self.nome, self.data, self.email, self.telefone, self.logradouro,
+                        self.endereco_numero, self.endereco_complemento, self.endereco_bairro, self.endereco_cidade, self.endereco_estado, self.endereco_cep,
+                        self.nome_responsavel,
+                        self.nascimento_responsavel, self.email_responsavel, self.telefone_responsavel, self.genero, self.lgbtqia,
+                        self.escolaridade, self.ocupacao, self.moradia, self.moradores, self.filhos, self.etnia, self.disponibilidade, self.renda, self.medida_altura,
+                        self.medida_busto, self.medida_cintura, self.medida_quadril, self.observacoes)
+
+    def test_modelo(self):
+        
+        self.assertEqual(self.modelo.nome, '%s' % self.nome)
+        self.assertEqual(self.modelo.data_nascimento, self.data)
+        self.assertEqual(self.modelo.email, self.email)
+        self.assertEqual(self.modelo.telefone, self.telefone)
+        self.assertEqual(self.modelo.endereco_logradouro, self.logradouro)
+        self.assertEqual(self.modelo.endereco_numero, self.endereco_numero)
+        self.assertEqual(self.modelo.endereco_complemento, self.endereco_complemento)
+        self.assertEqual(self.modelo.endereco_bairro, self.endereco_bairro)
+        self.assertEqual(self.modelo.endereco_cidade, self.endereco_cidade)
+        self.assertEqual(self.modelo.endereco_estado, self.endereco_estado)
+        self.assertEqual(self.modelo.endereco_cep, self.endereco_cep)
+        self.assertEqual(self.modelo.responsavel_nome, self.nome_responsavel)
+        self.assertEqual(self.modelo.responsavel_nascimento, self.nascimento_responsavel)
+        self.assertEqual(self.modelo.responsavel_email, self.email_responsavel)
+        self.assertEqual(self.modelo.responsavel_telefone, self.telefone_responsavel)
+        self.assertEqual(self.modelo.genero, self.genero)
+        self.assertEqual(self.modelo.lgbtqia, self.lgbtqia)
+        self.assertEqual(self.modelo.escolaridade, self.escolaridade)
+        self.assertEqual(self.modelo.ocupacao, self.ocupacao)
+        self.assertEqual(self.modelo.moradia, self.moradia)
+        self.assertEqual(self.modelo.moradores, self.moradores)
+        self.assertEqual(self.modelo.filhos, self.filhos)
+        self.assertEqual(self.modelo.etnia, self.etnia)
+        self.assertEqual(self.modelo.disponibilidade, self.disponibilidade)
+        self.assertEqual(self.modelo.renda, self.renda)
+        self.assertEqual(self.modelo.medida_altura, self.medida_altura)
+        self.assertEqual(self.modelo.medida_busto, self.medida_busto)
+        self.assertEqual(self.modelo.medida_cintura, self.medida_cintura)
+        self.assertEqual(self.modelo.medida_quadril, self.medida_quadril)
+        self.assertEqual(self.modelo.observacoes, self.observacoes)
 
     def test_calcula_idade(self):
-        modelo = Modelo('', '1993-07-09T03:00:00.000Z', '', '',
-                        '','','','','','','','', '', '','','','','','','','', '', '','','','','','','', '')
-        freezer = freeze_time("2019-09-26 12:00:01")
-        freezer.start()
-        idade = modelo.calcula_idade()
+        idade = self.modelo.calcula_idade()
         self.assertEqual(26, idade)
-        freezer.stop()
+
+    def teardown_method(self, test_method):
+        self.freezer.stop()
