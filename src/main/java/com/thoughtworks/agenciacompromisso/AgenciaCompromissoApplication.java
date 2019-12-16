856fdf2586851aa23c5e6bfd1c27201f72a6fec4
astenstrasser <astenstrasser@gmail.com>
2019-12-16 14:19:09 -0300

    Reestruturando o projeto para uso de javacom spring boot

---
+++ src/main/java/com/thoughtworks/agenciacompromisso/AgenciaCompromissoApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.agenciacompromisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaCompromissoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaCompromissoApplication.class, args);
+	}
+
+}
