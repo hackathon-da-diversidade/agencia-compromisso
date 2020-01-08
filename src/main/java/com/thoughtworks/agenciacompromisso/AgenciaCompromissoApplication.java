7ec26a2fc388797ab48fbe4d6db4536c425e6348
Lucas Veloso <lucas.s.veloso@hotmail.com>
2020-01-08 11:12:17 -0300

    Habilitando CORS

--- src/main/java/com/thoughtworks/agenciacompromisso/AgenciaCompromissoApplication.java
+++ src/main/java/com/thoughtworks/agenciacompromisso/AgenciaCompromissoApplication.java
@@ -2,12 +2,24 @@ package com.thoughtworks.agenciacompromisso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AgenciaCompromissoApplication {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AgenciaCompromissoApplication.class, args);
 	}
-
 }
