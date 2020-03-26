ca06983c45c59995190e01828615acce2bc58fe7
lalumabeserra <luma.beserra@thoughtworks.com>
2020-03-26 16:53:15 -0300

    [Luma, Stephano e Victoria][#32] Adding shoe size to fit model

--- src/main/java/com/thoughtworks/agenciacompromisso/models/Sizes.java
+++ src/main/java/com/thoughtworks/agenciacompromisso/models/Sizes.java
@@ -10,14 +10,17 @@ public class Sizes {
     private Double height;
     private String shirtSize;
     private int pantsSize;
+    private int shoeSize;
 
-    public Sizes(Double totalBustCircumference, Double totalWaistCircumference, Double totalHipCircumference, Double height, String shirtSize,  int pantsSize) {
+
+    public Sizes(Double totalBustCircumference, Double totalWaistCircumference, Double totalHipCircumference, Double height, String shirtSize,  int pantsSize, int shoeSize) {
         this.totalBustCircumference = totalBustCircumference;
         this.totalWaistCircumference = totalWaistCircumference;
         this.totalHipCircumference = totalHipCircumference;
         this.height = height;
         this.shirtSize = shirtSize;
         this.pantsSize = pantsSize;
+        this.shoeSize = shoeSize;
     }
 
     public Double getTotalWaistCircumference() {
