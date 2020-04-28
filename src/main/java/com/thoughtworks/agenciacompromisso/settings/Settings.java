ab340bb46acb018f556b5d095b0893a45cd4fe10
lalumabeserra <luma.beserra@thoughtworks.com>
2020-04-28 14:30:57 -0300

    [Luma e Stephano][#8] Renames filename

--- src/main/java/com/thoughtworks/agenciacompromisso/settings/Settings.java
+++ src/main/java/com/thoughtworks/agenciacompromisso/settings/Settings.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class Settings {
     @Value("${settings.cors.origins}")
     private String origins;
 
